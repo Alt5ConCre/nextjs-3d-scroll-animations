c58b5bad13e6773c99914f09f9831978eebcacdc
Alt5ConCre <150808511+Alt5ConCre@users.noreply.github.com>
2026-09-22 14:17:59 +0400

    Reset Burger project: clear old Burger homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
-import dynamic from "next/dynamic";
-const BurgerExperience=dynamic(()=>import("@/components/burger/BurgerExperience"),{ssr:false});
-export default function Home(){return <BurgerExperience/>;}
+export default function Home() {
+  return null;
+}
