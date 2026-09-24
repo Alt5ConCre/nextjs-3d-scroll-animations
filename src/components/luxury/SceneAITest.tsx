a87ac5fe751138ab1b8900171b7d7fcc6a736060
Alt5ConCre <150808511+Alt5ConCre@users.noreply.github.com>
2026-09-24 16:16:47 +0400

    fix: safely reintroduce subtle cinematic effects

--- src/components/luxury/SceneAITest.tsx
+++ src/components/luxury/SceneAITest.tsx
@@ -91,16 +91,16 @@ class PostFXBoundary extends React.Component<
 function CinematicGrade() {
   return (
     <PostFXBoundary>
-      <EffectComposer multisampling={0} frameBufferType={THREE.HalfFloatType}>
+      <EffectComposer multisampling={0}>
         <Bloom
-          luminanceThreshold={1.15}
-          luminanceSmoothing={0.55}
-          intensity={0.08}
-          radius={0.35}
+          luminanceThreshold={1.2}
+          luminanceSmoothing={0.7}
+          intensity={0.06}
+          radius={0.25}
           mipmapBlur
         />
-        <Noise premultiply opacity={0.012} />
-        <Vignette eskil={false} offset={0.28} darkness={0.48} />
+        <Noise premultiply opacity={0.008} />
+        <Vignette eskil={false} offset={0.3} darkness={0.42} />
       </EffectComposer>
     </PostFXBoundary>
   );
